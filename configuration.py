ab8f5c7a7764a36cfa12717b10861f365daa0a2d
Julio César Perez Zuñiga <julio_c1994p@hotmail.com>
2024-08-17 00:09:01 -0700

    Proyecto S7

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-82bceec8-09a7-427f-8f51-8fb654d210d9.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-405d8067-bb2f-428a-aa26-078ac5537097.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 KITS_PATH = "/api/v1/kits"
 CREATE_USER_PATH = "/api/v1/users"
